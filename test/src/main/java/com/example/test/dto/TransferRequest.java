6abdcb980ed99f4d28cbc94d452b3302216b526f
Alexey Utkin <alex_habar@mail.ru>
2024-02-28 06:37:14 +0300

    HW10

---
+++ test/src/main/java/com/example/test/dto/TransferRequest.java
@@ -0,0 +1,12 @@
+package com.example.test.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class TransferRequest {
+    private long senderAccountId;
+    private long receiverAccountId;
+    private BigDecimal amount;
+}
---
+++ test/src/main/java/com/example/test/exceptions/AccountNotFoundException.java
@@ -0,0 +1,4 @@
+package com.example.test.exceptions;
+
+public class AccountNotFoundException extends RuntimeException {
+}
